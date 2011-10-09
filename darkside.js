238e0f6690789cbdf2d043f4d05332a9a67c049f
Tom Barnard <tom@rumcode.com>
2011-10-09 11:51:06 +0100

    Changed how size of prism is calculated.

--- darkside.js
+++ darkside.js
@@ -211,6 +211,7 @@ function draw()
     ctx.fillStyle = "rgb(0, 0, 0)";
     ctx.fillRect(0, 0, width, height);
 
+    var x = 3/4 * Math.min(width, height);
     var x = Math.max(width/2.75, height/2.75);
 
     // Create prism
